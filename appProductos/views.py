fbaaaf0a70697e3ffab9633f9f0b085c88b1432e
Andresacer <andipipe2002@gmail.com>
2023-10-04 13:49:31 -0500

    Partes nuevas

--- appProductos/views.py
+++ appProductos/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render 
+from .models import * 
+
+# Create your views here. 
+def verCategorias(request): 
+    #Consultar categorias 
+    listaCateg = Categoria.objects.all() 
+    
+    #ensamblar context 
+    context = { 
+        'categorias': listaCateg, 
+        'titulo': 'Categorias de Productos del Supermercado', 
+    } 
+    
+    #renderizar return 
+    render(request, 'productos/categorias.html', context)
 
 # Create your views here.
